905a1b3ec1a7e3a5516f27e9a2dbbd6e42644d03
ewojciechowska <ewojciechowska@bones.studio>
2024-10-23 13:32:48 +0200

    initial commit

---
+++ analyzer.py
@@ -0,0 +1,5 @@
+# Za pomocą Pythona albo innego wybranego języka napisz program, który:
+# Monitoruje jeden z wybranych folderów pod kątem pojawiających się plików .txt.
+# Skrypt ma analizować pojawiające się pliki tekstowe (które dodajemy manualnie).
+# Jeżeli plik tekstowy, który pojawił się w folderze, zawiera w środku słowo MOCAP, skrypt powinien skopiować go do innego folderu.
+# Jeśli w drugim folderze pojawi się skopiowany plik, ma zostać automatycznie otwarty w notatniku.
