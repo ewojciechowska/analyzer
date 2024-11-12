529c10f7bffddf9b78b9a9cca63fb3ef5c10dfc4
ewojciechowska <ewojciechowska@bones.studio>
2024-11-12 11:09:30 +0100

    Working = detect event create - any type of file

--- analyzer.py
+++ analyzer.py
@@ -9,17 +9,37 @@ from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
+class OnWatch:
+    # set the directory to watch
+    watchDirectory = Path(r"D:\Random_files_space\test_analyzer")
+
+    def __init__(self):
+        self.observer = Observer()
+
+    def run(self):
+        event_handler = MyHandler()
+        self.observer.schedule(event_handler, self.watchDirectory, recursive = True)
+        self.observer.start()
+        try:
+            while True:
+                time.sleep(5)
+        except:
+            self.observer.stop()
+            print("Observer stopped!")
+
+        self.observer.join()
+
 class MyHandler(FileSystemEventHandler):
-    def on_created(self, event):
-        print("File {event.src_path} has been created")
+
+    @staticmethod
+    def on_any_event(event):
+        if event.is_directory:
+            return None
+
+        elif event.event_type == 'created':
+            print("Event occured: CREATED - % s." % event.src_path)
+
 
 if __name__ == "__main__":
-    event_handler = MyHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path= Path(r"D:\Random_files_space\test_analyzer"), recursive=True)
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+    watch = OnWatch()
+    watch.run()
